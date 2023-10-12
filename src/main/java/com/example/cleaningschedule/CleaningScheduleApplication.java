ab08ebdc1bbf6dbaea27a7d81cc539fdf9696491
AleFurgala <ale.furgala@gmail.com>
2023-10-12 20:15:49 +0200

    new project was created

---
+++ src/main/java/com/example/cleaningschedule/CleaningScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.example.cleaningschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleaningScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleaningScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cleaningschedule/CleaningScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cleaningschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleaningScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
